f6d548b358fb09c3a1b976fa7dc40a7fb17e20cd
everettmakes <jeverer@gmail.com>
2024-09-20 15:33:35 +0100

    nice css

--- portfolio-app/src/projects_grid.jsx
+++ portfolio-app/src/projects_grid.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "./projects.css";
+import scotlandYardImg from './scotland_yard.jpg';
+import mcaImg from './mca.jpg';
+import golImg from './gol.jpg';
+import kaggleImg from './kaggle.jpg';
 
 const projectData = [
   {
@@ -7,24 +11,28 @@ const projectData = [
     title: "Scotland Yard",
     description: "I completed this coursework in Java",
     url: "https://github.com/everettmakes/ScotlandYard/blob/main/report.pdf",
+    img: scotlandYardImg,
   },
   {
     id: 2,
     title: "Marine Conservation App",
     description: "My team came top in the year for our project",
     url: "https://marineconservationapp.com/",
+    img: mcaImg,
   },
   {
     id: 3,
     title: "Game of Life",
     description: "I completed this coursework in C",
     url: "https://github.com/everettmakes/GameOfLife/blob/main/report%20.pdf",
+    img: golImg,
   },
   {
     id: 4,
     title: "Kaggle",
     description: "Here you can see my data science activities on Kaggle",
     url: "https://www.kaggle.com/jevrett",
+    img: kaggleImg,
   },
 ];
 
@@ -39,8 +47,9 @@ const Project = ({ project }) => {
 
   return (
     <div
-      className={`project-item ${project.url === "private" ? "private" : ""}`}
+      className="project-item"
       onClick={handleClick}
+      style={{ backgroundImage: `url(${project.img})` }} // Apply the background image
       aria-label={`View details of ${project.title}`}
     >
       <h3>{project.title}</h3>
@@ -48,6 +57,8 @@ const Project = ({ project }) => {
     </div>
   );
 };
+
+
 
 const ProjectsGrid = () => {
   return (
